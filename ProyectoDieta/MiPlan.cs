fcd9b37cf3dd20fcc2e7e97e9cc5c3d37c33858a
Emanuel Ramirez <42881151+emabitback@users.noreply.github.com>
2019-05-22 21:57:44 -0500

    Mucho TODO
    ->DatosConexion.cs ahí ahora asignas la cadena de conexión en lugar de estar colocándola en cada form
    falta:
    validar campos de texto
    comprobar que no se intente repetir un alimento o asignar una dieta repetida

---
+++ ProyectoDieta/MiPlan.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+namespace ProyectoDieta
+{
+    public partial class MiPlan : Form
+    {
+        static string cadena_conexion = DatosConexion.cadena;         
+
+        string correo;
+        public MiPlan(string correo)
+        {
+            this.correo = correo;
+            InitializeComponent();
+        }
+
+        private void MiPlan_Load(object sender, EventArgs e)
+        {
+            MySqlConnection conexion = new MySqlConnection();
+            conexion.ConnectionString = cadena_conexion;
+            MySqlCommand comd = new MySqlCommand("SELECT * FROM dieta WHERE correo = ?correo", conexion);
+            comd.Parameters.AddWithValue("?correo", correo);
+            List_Lunes.Items.Clear();
+            List_Martes.Items.Clear();
+            List_Miercoles.Items.Clear();
+            List_Jueves.Items.Clear();
+            List_Viernes.Items.Clear();
+            List_Sabado.Items.Clear();       
+
+            try
+            {
+                conexion.Open();
+                MySqlDataReader lector = comd.ExecuteReader();
+                //llenando la list_box
+                if (lector.HasRows)
+                    while (lector.Read())
+                    {
+                        if (lector[1].ToString().Equals("Lunes"))
+                        {
+                            List_Lunes.Items.Add(lector[2]);                        
+                        }
+                        if (lector[1].ToString().Equals("Martes"))
+                        {
+                            List_Martes.Items.Add(lector[2]); 
+                        }
+                        if (lector[1].ToString().Equals("Miércoles"))
+                        {
+                            List_Miercoles.Items.Add(lector[2]); 
+                        }
+                        if (lector[1].ToString().Equals("Jueves"))
+                        {
+                            List_Jueves.Items.Add(lector[2]); 
+                        }
+                        if (lector[1].ToString().Equals("Viernes"))
+                        {
+                            List_Viernes.Items.Add(lector[2]); 
+                        }
+                        if (lector[1].ToString().Equals("Sábado"))
+                        {
+                            List_Sabado.Items.Add(lector[2]); 
+                        }
+                    }                   
+                conexion.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("ERROR: " + ex);
+            }
+        }
+    }
+}
---
+++ ProyectoDieta/RecuperaContra.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+namespace ProyectoDieta
+{
+    public partial class RecuperaContra : Form
+    {
+        static string cadena_conexion = DatosConexion.cadena;         
+
+        public RecuperaContra()
+        {
+            InitializeComponent();
+        }
+
+        private void materialFlatButton1_Click(object sender, EventArgs e)
+        {
+            MySqlConnection conexion = new MySqlConnection();
+            conexion.ConnectionString = cadena_conexion;
+            string correo = txt_correo.Text;
+            MySqlCommand comd = new MySqlCommand("SELECT pregunta,respuesta FROM usrs_login WHERE correo = ?correo");
+            comd.Parameters.AddWithValue("?correo", correo);
+            comd.Connection = conexion;
+            if (conexion.State == ConnectionState.Closed)
+                conexion.Open();
+            MySqlDataReader lector = comd.ExecuteReader();
+            try
+            {
+                lector.Read();
+                if (lector.HasRows)
+                {
+                    lbl_pregunta.Text = lector[0].ToString();
+                }
+                else
+                {
+                    MessageBox.Show("El dato dado no se encuentra registrado");
+                }
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("Se ha producido un error" + err + ".");
+            }
+           
+        }
+
+        private void btn_recuperar_Click(object sender, EventArgs e)
+        {
+            MySqlConnection conexion = new MySqlConnection();
+            conexion.ConnectionString = cadena_conexion;
+            string respuesta = txt_respuesta.Text;
+            string correo = txt_correo.Text;
+            MySqlCommand comd = new MySqlCommand("SELECT correo,psw,rol,pregunta,respuesta, CONVERT(AES_DECRYPT(psw, respuesta), CHARACTER)  FROM usrs_login WHERE correo = ?correo");
+            comd.Parameters.AddWithValue("?correo", correo);
+            comd.Connection = conexion;
+            if (conexion.State == ConnectionState.Closed)
+                conexion.Open();
+            MySqlDataReader lector = comd.ExecuteReader();
+            try
+            {
+                if (lector.HasRows)
+                {
+                    lector.Read();
+                    if (respuesta == lector[4].ToString())
+                    {
+                        MessageBox.Show("Tu contraseña es la siguiente:" + lector[5].ToString());
+                    }
+                    else
+                    {
+                        MessageBox.Show("Los valores dados no coinciden.");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("El dato dado no se encuentra registrado");
+                }
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("Se ha producido un error" + err + ".");
+            }
+        }
+
+        private void RecuperaContra_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
